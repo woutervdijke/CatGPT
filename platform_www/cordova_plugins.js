296bfd4d6760e1feb42b35b7077ea03d7ac457c2
rasvanjaya21 <mrsejati17@gmail.com>
2023-03-06 23:07:28 +0700

    add admob plugin, hura !

--- platform_www/cordova_plugins.js
+++ platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
+      "id": "admob-plus-cordova.AdMob",
+      "file": "plugins/admob-plus-cordova/www/admob.js",
+      "pluginId": "admob-plus-cordova",
+      "clobbers": [
+        "admob"
+      ]
+    },
+    {
       "id": "cordova-plugin-splashscreen.SplashScreen",
       "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
       "pluginId": "cordova-plugin-splashscreen",
@@ -10,6 +18,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
+    "admob-plus-cordova": "1.28.0",
     "cordova-plugin-proguard": "2.2.0",
     "cordova-plugin-splashscreen": "6.0.2"
   };
